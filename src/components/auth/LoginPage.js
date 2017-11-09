c439f319208d80fe761511ca232d504e8179a636
Cédric Oloa <cedric.oloa@laposte.net>
2017-11-09 12:32:08 +0100

    first Commit

---
+++ src/components/auth/LoginPage.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import OktaSignIn from '@okta/okta-signin-widget';
+
+export default class LoginPage extends React.Component{
+  constructor(){
+    super();
+    this.state = {user:null};
+    this.widget = new OktaSignIn({
+      baseUrl: 'https://dev-432408-admin.oktapreview.com',
+      clientId: '0oacrsc7ftRAf5so20h7',
+      redirectUri: 'http://localhost:3000',
+      authParams: {
+        responseType: 'id_token'
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.widget.session.get((response) => {
+      if(response.status !== 'INACTIVE'){
+        this.setState({user:response.login});
+      }else{
+        this.showLogin();
+      }
+    });
+  }
+
+  showLogin(){
+    Backbone.history.stop();
+    this.widget.renderEl({el:this.loginContainer},
+      (response) => {
+        this.setState({user: response.claims.email});
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  logout(){
+    this.widget.signOut(() => {
+      this.setState({user: null});
+      this.showLogin();
+    });
+  }
+  
+  render(){
+    return(
+      <div>
+      {this.state.user ? (
+        <div className="container">
+        <div>Welcome, {this.state.user}!</div>
+        <button onClick={this.logout}>Logout</button>
+        </div>
+      ) : null}
+      {this.state.user ? null : (
+        <div ref={(div) => {this.loginContainer = div; }} />
+      )}
+      </div>
+    );
+  }
+}
